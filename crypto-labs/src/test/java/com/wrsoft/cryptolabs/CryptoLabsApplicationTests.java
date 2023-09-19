c163e1b7239430eaa4fbf7050714525866074c6c
hichang4u <kbaysin@hanmail.net>
2023-09-19 13:08:30 +0900

    first commit!!

---
+++ crypto-labs/src/test/java/com/wrsoft/cryptolabs/CryptoLabsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wrsoft.cryptolabs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoLabsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
